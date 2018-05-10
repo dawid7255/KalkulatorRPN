2b5069ecc33f7efab99fb8f20bd157d323f2b635
dawid7255 <33196440+dawid7255@users.noreply.github.com>
2018-05-10 15:22:03 +0200

    Over

--- app/src/main/java/com/example/dawid/kalkulatorrpn/MainActivity.kt
+++ app/src/main/java/com/example/dawid/kalkulatorrpn/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.dawid.kalkulatorrpn
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,7 +11,7 @@ import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
 
-    private var NumerStosu: Deque<Double> = ArrayDeque<Double>()
+    private var Stos: Deque<Double> = ArrayDeque<Double>()
     private var historia: Deque<Deque<Double>> = ArrayDeque<Deque<Double>>()
     private var buffor=""
 
@@ -19,29 +20,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun PokazEkran(isUndo: Boolean): String {
-        var text = ""
-        var i = NumerStosu.count()
+    fun PokazEkran(Cofaj: Boolean): String {
+        var ekran = ""
+        var i = Stos.count()
 
-        NumerStosu.forEach() {
-            text += i.toString() + ":   "  + String.format("%." + 2 +"f",it) + "\n"
+        Stos.forEach() {
+            ekran += i.toString() + ":   "  + String.format("%." + 2 +"f",it) + "\n"
             i--
         }
-        if(!isUndo){
+        if(!Cofaj){
             if(historia.count() > 20)
             {
                 historia.removeLast()
             }
-            var tmp : Deque<Double> = ArrayDeque<Double>(NumerStosu)
+            var tmp : Deque<Double> = ArrayDeque<Double>(Stos)
             historia.push(tmp)
         }
-        return text
+        return ekran
     }
 
-    private fun OdswiezStos(isUndo  : Boolean = false){
+    private fun OdswiezStos(Cofaj  : Boolean = false){
         TextViewBuffor.text = buffor
-        ilosc.text = "Ilosc: "+(NumerStosu.count()).toString()
-        Stos.text = PokazEkran(isUndo)
+        ilosc.text = "Count: "+(Stos.count()).toString()
+        EkranStos.text = PokazEkran(Cofaj)
     }
 
     fun Jeden(v: View){
@@ -97,10 +98,12 @@ class MainActivity : AppCompatActivity() {
     fun Kropka(v: View){
         if(!buffor.contains("."))
         {
-            buffor += "."
-        }
-        if(buffor.isNullOrEmpty()){
-            buffor += "0."
+            if(buffor.isNullOrEmpty()){
+                buffor += "0."
+            }
+            else{
+                buffor += "."
+            }
         }
         TextViewBuffor.text = buffor
     }
@@ -108,13 +111,19 @@ class MainActivity : AppCompatActivity() {
     fun Enter(v: View){
         if(buffor.isNullOrEmpty())
         {
-            if(NumerStosu.count() == 0)
+            if(Stos.count() == 0)
             {
                 OdswiezStos()
                 return
             }
-            var ostatni = NumerStosu.first()
-            NumerStosu.push(ostatni)
+            var ostatni = Stos.first()
+            Stos.push(ostatni)
+            OdswiezStos()
+            return
+        }
+
+        if(buffor=="."){
+            buffor=""
             OdswiezStos()
             return
         }
@@ -123,21 +132,25 @@ class MainActivity : AppCompatActivity() {
         if(liczba == null) {
             throw NullPointerException()
         }
-        NumerStosu.push(liczba)
+        Stos.push(liczba)
         buffor = ""
         OdswiezStos()
     }
 
     fun UsunZStosu(v: View){
-        if((NumerStosu.count())>0) {
-            NumerStosu.removeFirst()
+        if((Stos.count())>0) {
+            Stos.removeFirst()
             OdswiezStos()
         }
     }
 
     fun AC(v: View){
-        if((NumerStosu.count())>0) {
-            NumerStosu.clear()
+        if((Stos.count())>0) {
+            Stos.clear()
+            buffor = ""
+            OdswiezStos()
+        }
+        if((Stos.count())==0) {
             buffor = ""
             OdswiezStos()
         }
@@ -154,107 +167,140 @@ class MainActivity : AppCompatActivity() {
         if((historia.count())>1) {
             historia.removeFirst()
             var tmp: Deque<Double> = ArrayDeque<Double>(historia.first())
-            NumerStosu = tmp
+            Stos = tmp
             OdswiezStos(true)
+        }
+        else{
+            BrakHistori()
         }
     }
 
     fun Zamien(v: View){
-        if((NumerStosu.count())>1) {
-            var ostatni = NumerStosu.first()
-            NumerStosu.removeFirst()
+        if((Stos.count())>1) {
+            var ostatni = Stos.first()
+            Stos.removeFirst()
 
-            var przedostatni = NumerStosu.first()
-            NumerStosu.removeFirst()
+            var przedostatni = Stos.first()
+            Stos.removeFirst()
 
-            NumerStosu.push(ostatni)
-            NumerStosu.push(przedostatni)
+            Stos.push(ostatni)
+            Stos.push(przedostatni)
             OdswiezStos()
+        }
+        else{
+            MaloLiczb()
         }
     }
 
     fun Znak(v: View){
-        if((NumerStosu.count())>0) {
-            var liczba = NumerStosu.first()
-            NumerStosu.removeFirst()
+        if (Stos.count()<1){
+            MaloLiczb()
+        }
+        if((Stos.count())>0) {
+            var liczba = Stos.first()
+            Stos.removeFirst()
             liczba = liczba * -1
-            NumerStosu.addFirst(liczba)
+            Stos.push(liczba)
             OdswiezStos()
         }
     }
 
     fun Suma(v: View){
-        DzialaniaNaStosie(Operacja.suma)
+        if (Stos.count()<2){
+            MaloLiczb()
+            return
+        }
+        var ostatni = Stos.first()
+        Stos.removeFirst()
+        var przedostatni = Stos.first()
+        Stos.removeFirst()
+        Stos.push(przedostatni + ostatni)
         OdswiezStos()
     }
 
     fun Odejmowanie(v: View){
-        DzialaniaNaStosie(Operacja.odejmowanie)
+        if (Stos.count()<2){
+            MaloLiczb()
+            return
+        }
+        var ostatni = Stos.first()
+        Stos.removeFirst()
+        var przedostatni = Stos.first()
+        Stos.removeFirst()
+        Stos.push(przedostatni - ostatni)
         OdswiezStos()
     }
 
     fun Mnozenie(v: View){
-        DzialaniaNaStosie(Operacja.mnozenie)
+        if (Stos.count()<2){
+            MaloLiczb()
+            return
+        }
+        var ostatni = Stos.first()
+        Stos.removeFirst()
+        var przedostatni = Stos.first()
+        Stos.removeFirst()
+        Stos.push(przedostatni * ostatni)
         OdswiezStos()
     }
 
     fun Dzielenie(v: View){
-        DzialaniaNaStosie(Operacja.dzielenie)
+        if (Stos.count()<2){
+            MaloLiczb()
+            return
+        }
+        var ostatni = Stos.first()
+        Stos.removeFirst()
+        var przedostatni = Stos.first()
+        Stos.removeFirst()
+        Stos.push(przedostatni / ostatni)
         OdswiezStos()
     }
 
     fun Potega(v: View){
-        DzialaniaNaStosie(Operacja.potega)
+        if (Stos.count()<2){
+            MaloLiczb()
+            return
+        }
+        var ostatni = Stos.first()
+        Stos.removeFirst()
+        var przedostatni = Stos.first()
+        Stos.removeFirst()
+        Stos.push(przedostatni.pow(ostatni))
         OdswiezStos()
     }
 
     fun Pierwiastek(v: View){
-        DzialaniaNaStosie(Operacja.pierwiastek)
+        if (Stos.count()<1){
+            MaloLiczb()
+            return
+        }
+        var ostatni = Stos.first()
+        if (ostatni<0){
+            UjemnaLiczba()
+            return
+        }
+        Stos.removeFirst()
+        Stos.push(sqrt(ostatni))
         OdswiezStos()
     }
 
-    fun DzialaniaNaStosie(typeOperation: Operacja){
-        val Wynik : Double
-
-        if(NumerStosu.count() < 2 || typeOperation == Operacja.pierwiastek)
-        {
-            return
-        }
-        var ostatni = NumerStosu.first()
-        NumerStosu.removeFirst()
-
-        var przedostatni = NumerStosu.first()
-        NumerStosu.removeFirst()
-
-
-        when(typeOperation){
-            Operacja.suma-> {
-                Wynik =  przedostatni + ostatni
-            }
-            Operacja.odejmowanie->{
-                Wynik =  przedostatni - ostatni
-            }
-            Operacja.mnozenie->{
-                Wynik = przedostatni * ostatni
-            }
-            Operacja.dzielenie ->{
-                Wynik = przedostatni / ostatni
-            }
-            Operacja.potega ->{
-                Wynik = przedostatni.pow(ostatni)
-            }
-            Operacja.pierwiastek ->{
-                NumerStosu.push(przedostatni)
-                Wynik = sqrt(ostatni)
-            }
-            else ->{
-                Wynik = 0.0
-            }
-        }
-        NumerStosu.push(Wynik)
+    fun UjemnaLiczba() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setTitle("Error!")
+        dialogBuilder.setMessage("The number is negative!")
+        dialogBuilder.create().show()
     }
-}
-
-public enum class Operacja {
-    suma, odejmowanie, mnozenie, dzielenie, potega, pierwiastek
+    fun MaloLiczb() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setTitle("Error!")
+        dialogBuilder.setMessage("Not enough numbers on the stack!")
+        dialogBuilder.create().show()
+    }
+    fun BrakHistori() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setTitle("Error!")
+        dialogBuilder.setMessage("No history!")
+        dialogBuilder.create().show()
+    }
 }
